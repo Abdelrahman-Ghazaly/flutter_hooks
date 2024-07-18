f1a413a09040adc71db10a2908036eb01980dc0a
Abdelrahman Ghazaly <ghazaly.abdelrahman@gmail.com>
2024-07-18 18:21:54 +0300

    step-4

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 extension CompactMap<T> on Iterable<T?> {
@@ -19,19 +20,14 @@ class MainApp extends HookWidget {
   const MainApp({super.key});
   @override
   Widget build(BuildContext context) {
-    final future = useMemoized(() => NetworkAssetBundle(Uri.parse(imageUrl))
-        .load(imageUrl)
-        .then((value) => value.buffer.asUint8List())
-        .then((value) => Image.memory(value)));
-    final snapShot = useFuture(future);
-
+    final countDown = useMemoized(() => CountDown(from: 20));
+    final notifier = useListenable(countDown);
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [snapShot.data].compactMap().toList(),
+        body: Center(
+          child: Text(
+            notifier.value.toString(),
           ),
         ),
       ),
@@ -39,5 +35,16 @@ class MainApp extends HookWidget {
   }
 }
 
-const String imageUrl =
-    'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg';
+class CountDown extends ValueNotifier<int> {
+  CountDown({required int from}) : super(from) {
+    sub = Stream.periodic(const Duration(seconds: 1), (value) => from - value)
+        .takeWhile((value) => value >= 0)
+        .listen((value) => this.value = value);
+  }
+  late StreamSubscription sub;
+  @override
+  void dispose() {
+    sub.cancel();
+    super.dispose();
+  }
+}
