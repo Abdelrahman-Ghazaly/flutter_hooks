f89e1e15a4726bf054f24854209fd4f90566a527
Abdelrahman Ghazaly <ghazaly.abdelrahman@gmail.com>
2024-07-18 18:06:20 +0300

    step-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> compactMap<E>([
+    E? Function(T?)? transform,
+  ]) =>
+      map(
+        transform ?? (e) => e,
+      ).where((e) => e != null).cast();
+}
 
 void main() {
   runApp(const MainApp());
@@ -9,19 +19,11 @@ class MainApp extends HookWidget {
   const MainApp({super.key});
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    final controller2 = useTextEditingController();
-    final text = useState("");
-
-    useEffect(
-      () {
-        controller.addListener(() {
-          text.value = controller.text;
-        });
-        return null;
-      },
-      [controller, controller2],
-    );
+    final future = useMemoized(() => NetworkAssetBundle(Uri.parse(imageUrl))
+        .load(imageUrl)
+        .then((value) => value.buffer.asUint8List())
+        .then((value) => Image.memory(value)));
+    final snapShot = useFuture(future);
 
     return MaterialApp(
       home: Scaffold(
@@ -29,14 +31,13 @@ class MainApp extends HookWidget {
         body: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
-            children: [
-              TextField(controller: controller),
-              TextField(controller: controller2),
-              Text('Text you typed in the first field: ${text.value}'),
-            ],
+            children: [snapShot.data].compactMap().toList(),
           ),
         ),
       ),
     );
   }
 }
+
+const String imageUrl =
+    'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg';
