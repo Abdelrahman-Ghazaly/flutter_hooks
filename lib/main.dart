1d4c7f723e174b20963f3df5b06cf7f84f0ad30a
Abdelrahman Ghazaly <ghazaly.abdelrahman@gmail.com>
2024-07-18 17:09:23 +0300

    step-1

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+Stream<String> getTime() => Stream.periodic(
+      const Duration(seconds: 1),
+      (_) => DateTime.now().toIso8601String(),
+    );
 
+class MainApp extends HookWidget {
+  const MainApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final time = useStream(getTime());
+
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
+        appBar: AppBar(
+          title: Text(time.data ?? ''),
+        ),
+        body: const Center(
           child: Text('Hello World!'),
         ),
       ),
